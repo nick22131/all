0cfbf00deefaf1eef192f955b9c9925d456a9a50
nick22131 <nnr224@gmail.com>
2025-07-19 20:40:16 +0530

    win lap

---
+++ New folder/Practice problem/src/p010_ReversingStringUsingStringBuilder.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class p010_ReversingStringUsingStringBuilder {
+    public static void main(String[] args ){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a String to Reverse: ");
+        String input = sc.next();
+        StringBuilder sb = new StringBuilder(input);
+        String rev = sb.reverse().toString();
+        System.out.print("\n Reverse String of Given Input is : " + rev);
+
+    }
+}
