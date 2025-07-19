10eccb64929923c1b3db76e37a622651a2886573
nick22131 <nnr224@gmail.com>
2025-07-19 12:59:10 +0530

    win lap

---
+++ New folder/Practice problem/src/p004_CountNumberOfDigits.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class p004_CountNumberOfDigits {
+    public static void main(String[] args ){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter a number : ");
+        int input = sc.nextInt();
+
+
+    }
+}
