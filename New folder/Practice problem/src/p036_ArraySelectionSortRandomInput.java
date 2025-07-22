181e37836d56ea513edd4a70701dab5efc5fb80a
Nick <nnr224@gmail.com>
2025-07-23 04:42:00 +0530

    win pc

--- New folder/Practice problem/src/p036_ArraySelectionSortRandomInput.java
+++ New folder/Practice problem/src/p036_ArraySelectionSortRandomInput.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -8,8 +9,30 @@ public class p036_ArraySelectionSortRandomInput {
         int input = sc.nextInt();
         Random ran = new Random();
         int[] array = new int[input];
+        long t1 = System.currentTimeMillis();
         for(int i = 0 ; i < input ; i++){
           array[i] = ran.nextInt(1000);
+        }
+        System.out.print("\n Array Before Sorting: " + Arrays.toString(array));
+        selectionSort(array);
+        System.out.print("\n After Sorting : " + Arrays.toString(array));
+        long t2 = System.currentTimeMillis();
+        System.out.print("\n Time taken : "+ (t2 - t1));
+    }
+    static void selectionSort(int[] array){
+        int size = array.length;
+        int tem = 0;
+        int minIndex = 0 ;
+        for(int i = 0; i < size ; i++ ){
+            minIndex = i;
+            for(int j = i+1 ; j < size ; j++)  {
+                if (array[minIndex] > array[j]) {
+                    minIndex = j;
+                }
+            }
+            tem = array[minIndex];
+            array[minIndex] = array[i];
+            array[i] = tem;
         }
     }
 }
---
+++ New folder/Practice problem/src/p040_LinearSearchArray.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class p040_LinearSearchArray {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of Array: ");
+        int size = sc.nextInt();
+        int[] array = new int[size];
+        
+    }
+}
