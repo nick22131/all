43d023a65f94380248804d07ced0e9706237f6a1
Nick <nnr224@gmail.com>
2025-07-23 20:31:07 +0530

    win pc

---
+++ New folder/Practice problem/src/p045_WordCount.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class p045_WordCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter sentence to count words: ");
+        String input = sc.nextLine();
+        int count = 1;
+        for (int i = 0; i < input.length()-1; i++){
+            if((input.charAt(i)== ' ') && (input.charAt(i+1) != ' ')){
+                count++;
+            }
+        }
+        System.out.print("\n Number of words r : " + count);
+    }
+
+}
