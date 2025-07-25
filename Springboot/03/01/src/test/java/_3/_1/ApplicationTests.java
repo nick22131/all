b08479ed124dccf89df81a3a9813e235f675e118
Nick <nnr224@gmail.com>
2025-07-25 23:38:10 +0530

    win pc

---
+++ Springboot/03/01/src/test/java/_3/_1/ApplicationTests.java
@@ -0,0 +1,13 @@
+package _3._1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
