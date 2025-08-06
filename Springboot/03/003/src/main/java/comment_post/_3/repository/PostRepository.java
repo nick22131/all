ae924ea2b869bf193845193df70e866b7c3c704c
nick22131 <nnr224@gmail.com>
2025-08-07 04:29:40 +0530

    win lap

--- Springboot/03/003/src/main/java/comment_post/_3/repository/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package comment_post._3.repository;
-
-import comment_post._3.entity.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface PostRepository extends JpaRepository<Post,Long> {
-}
