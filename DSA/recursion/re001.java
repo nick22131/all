a732d71589f8f6ee73e49231bda2c7182a213660
nick22131 <nnr224@gmail.com>
2025-09-23 09:19:14 +0530

    lap

--- DSA/recursion/re001.java
+++ DSA/recursion/re001.java
@@ -3,7 +3,7 @@ package recursion;
 public class re001 {
     public static void main(String[] args) {
 
-        printf(7);
+        printfBoth(7);
     }
 
     static void printf(int n){
@@ -12,5 +12,22 @@ public class re001 {
         }
         System.out.println(n);
         printf(n-1);
+    }
+
+    static void printfRev(int n ){
+        if(n == 0){
+            return;
+        }
+        printfRev(n-1);
+        System.out.println(n);
+    }
+
+    static void printfBoth(int n){
+        if(n == 0){
+            return;
+        }
+        System.out.println(n);
+        printfBoth(n-1);
+        System.out.println(n);
     }
 }
